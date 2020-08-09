392361f7aa465241bc9728a359de5cf4c96fa75d
Dominique Brisco <dbrisco93@gmail.com>
2020-08-09 07:55:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0
+  new_array = []
   while count < array.length do 
-    puts array[index]
+    puts new_array.push(array.index)
     count += 1
   end
 end
